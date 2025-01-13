d7f4f6d374c592c39b70698ab969de9a10d3cfd0
jesus <jesuslopj0@gmail.com>
2025-01-13 21:07:55 +0100

    Commit de prueba

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
     <>
       <Navbar />
       <h1>Weird Things</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsum minus excepturi nam repellat error ullam, eveniet natus quaerat accusamus in quam omnis amet quisquam? Saepe dicta non iure obcaecati?</p>
     </>
   )
 }
