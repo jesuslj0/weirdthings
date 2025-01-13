483551a5bacb215dbc794455f555309e65c511ac
jesus <jesuslopj0@gmail.com>
2025-01-13 20:57:02 +0100

    Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Navbar } from './Components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>Weird Things</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,17 @@
+import "../css/Navbar.css"
+
+export const Navbar = () => {
+    return (
+        <div id="navbar-container">
+            <nav id="navbar">
+                <ul>
+                    <li><a href="">Random Drawings</a></li>
+                    <li><a href="">Portraits</a></li>
+                    <li><a href="">Buildings</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">About</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
