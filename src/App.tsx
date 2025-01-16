4bbb53e0105fa6ff77efcca80dd7bb5a8bf17e4e
jesus <jesuslopj0@gmail.com>
2025-01-16 04:36:40 +0100

    Cambiando disposicion de random drawings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import './css/BackgroundVideo.css'
 import { Providers } from './Providers/Providers' //Proveedor de rutar
-import './css/fontfaces.css';
 
 function App() {
   return (
--- src/pages/RandomDrawings.tsx
+++ src/pages/RandomDrawings.tsx
@@ -1,28 +1,29 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import BackgroundVideo from "../Components/BackgroundVideo";
 import { Navbar } from "../Components/Navbar";
-import ImageCarousel from "../Components/ImageCarousel";
 
 import '../css/RandomDrawings.css';
 
 const RandomDrawingsContent = () => {
-    const [images, setImages] = useState<string[]>([]);
-    useEffect(() => {
-        setImages(
-            [
-            "/static/img/drawings/cara_triste.jpg",
-            "/static/img/drawings/calle_ciudad.jpg",
-            "/static/img/drawings/tio_raro.jpg",
-            "/static/img/drawings/tio_loco.jpg",
-            ] 
-        )
-    }, [])
+    const [images] = useState<string[]>([
+        "/static/img/drawings/cara_triste.jpg",
+        "/static/img/drawings/calle_ciudad.jpg",
+        "/static/img/drawings/free_guy.jpg",
+        "/static/img/drawings/hombre_ojo.jpg",
+        "/static/img/drawings/tio_raro.jpg",
+        "/static/img/drawings/black_hole.jpg",
+        "/static/img/drawings/tatto_coleccion.jpg",
+    ]);
 
     return (
         <>
             <Navbar />
             <h1 className="subtitle">Some random drawings</h1>
-            <ImageCarousel images={images}/>
+            <div id="images-container">
+                {images.map((src, index) => (
+                    <img key={index} src={src} alt={`Image ${index}`} />
+                ))}
+            </div>
         </>
     )
 }
